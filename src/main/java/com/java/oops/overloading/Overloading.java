375c1fd77b1f4b9f6127e2e0b079e4b44ab43e38
Kunal Ashar <kunal.ashar@zycus.com>
2018-10-19 18:06:58 +0530

    overloading code added

--- src/main/java/com/java/oops/overloading/Overloading.java
+++ src/main/java/com/java/oops/overloading/Overloading.java
@@ -2,10 +2,20 @@ package com.java.oops.overloading;
 
 public class Overloading
 {
+	// 1. Two methods are said to be overloaded if and only if both methods have same name but different argument types.
+
+	// 2. In C, method overloading concept is not available. Hence, we can't declare multiple methods with same name but
+	// different args types. If there is change in argument type compulsory we should go for new method name which
+	// increases complexity of programming.
+
+	// 3. In Java, we can declare multiple methods with same name but different arguments types, such types of methods
+	// are called overloaded methods.
+	
+	// 4. Having overloading concept in java reduces complexity of programming.
 
 	public static void main(String[] args)
 	{
-
+		
 	}
 
 }
