915c435f4d3b1af07b17c8c518ab4c46140f5e7b
Kunal Ashar <kunal.ashar@zycus.com>
2018-10-19 17:23:30 +0530

    has a relationship code added

--- src/main/java/com/java/oops/inheritance/HasARelationship.java
+++ src/main/java/com/java/oops/inheritance/HasARelationship.java
@@ -39,3 +39,17 @@ class Engine
 // Example: Department consist of several professors, without existing department there may be a chance of existing
 // professor objects. Hence, department and professor objects are weakly associated and this weak association is nothing
 // but aggregation.
+
+// Note:
+// 1. In Composition, objects are strongly associated whereas in aggregation, objects are weakly associated.
+
+// 2. In Composition, container object holds directly contained objects whereas in aggregation, container object holds
+// just references of contained objects.
+
+/** Is-A vs Has-A **/
+// 1. If we want total functionality of a class automatically then we should go for Is-A relationship.
+// Example: Complete functionality of class Person is required in class Student.
+
+// 2. If we want part of the functionality then we should go for Has-A relationship.
+// Example 100 methods are present in class Test but only few methods are in another class then go for Has-A
+// relationship.
