188db79aa2b9ee6ac03876556de14f0f488069ce
Kunal Ashar <kunal.ashar@zycus.com>
2018-10-19 17:02:10 +0530

    has a relationship code added

---
+++ src/main/java/com/java/oops/inheritance/HasARelationship.java
@@ -0,0 +1,34 @@
+package com.java.oops.inheritance;
+
+public class HasARelationship
+{
+	// 1. Has-A Relationship is also known as Composition or Aggregation.
+
+	// 2. There is not specific keyword to implement Has-A relation but most of the times we're dependent on new
+	// keyword.
+
+	// 3. The main advantage of Has-A relationship is reusability of the code.
+}
+
+// Example
+class Car
+{
+	public static void main(String[] args)
+	{
+		Engine e = new Engine();
+		// Class car Has-A Engine reference.
+	}
+}
+
+class Engine
+{
+
+}
+
+/** Difference between Composition and Aggregation **/
+// 1. Without existing container object, if there is no chance of existing contained objects then container and
+// contained objects are strongly associated and these strong association is nothing but composition.
+
+// Example: University consist of several departments, without existing university there is no change of existing
+// department. Hence, university and departments are strongly associated and this strong association is nothing but
+// composition.
