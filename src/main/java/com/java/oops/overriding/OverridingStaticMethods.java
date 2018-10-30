f352224ccbb384d7854866fd46c17a1d411251c2
Kunal Ashar <kunal.ashar@zycus.com>
2018-10-30 17:55:42 +0530

    Overriding code added

--- src/main/java/com/java/oops/overriding/OverridingStaticMethods.java
+++ src/main/java/com/java/oops/overriding/OverridingStaticMethods.java
@@ -4,3 +4,35 @@ public class OverridingStaticMethods
 {
 
 }
+
+// 1. We can't override a static method as non-static otherwise we'll get compile time error.
+
+// 2. If both parent and child class method are static then we won't get any compile time error.It seems overriding
+// concept applicable for static methods but it is not overriding, it is method hiding.
+
+class P3
+{
+	public static void m1()
+	{
+
+	}
+
+	public static void m2()
+	{
+
+	}
+}
+
+class C3 extends P3
+{
+	/*
+	 * public void m1() { }
+	 */
+
+	// CE: m1() in C3 cannot override m1() in P3; overriding method is static
+
+	public static void m2()
+	{
+
+	}
+}
