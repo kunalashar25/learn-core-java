b93d23b447f0caf067013b1bb09cce5c971ea6f0
Kunal Ashar <kunal.ashar@zycus.com>
2018-11-05 17:25:19 +0530

    Code added for Overriding

--- src/main/java/com/java/oops/overriding/OverridingVariables.java
+++ src/main/java/com/java/oops/overriding/OverridingVariables.java
@@ -2,10 +2,30 @@ package com.java.oops.overriding;
 
 public class OverridingVariables
 {
+	// 1. Variable resolution is always taken care by compiler based on referenced type irrespective of whether the
+	// variable is static or non-static.
+	
+	// 2. Overriding concept applicable only for methods but not for variables.
 
 	public static void main(String[] args)
 	{
+		Parent3 p = new Parent3();
+		System.out.println(p.x); // Output: 888
 
+		Child3 c = new Child3();
+		System.out.println(c.x); // Output: 999
+
+		Parent3 p1 = new Child3();
+		System.out.println(p1.x); // Output: 888
 	}
+}
 
+class Parent3
+{
+	int x = 888;
+}
+
+class Child3 extends Parent3
+{
+	int x = 999;
 }
