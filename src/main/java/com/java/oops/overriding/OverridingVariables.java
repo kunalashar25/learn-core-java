3844f344ff300c843920ef12110f198d7a3d0f8b
Kunal Ashar <kunal.ashar@zycus.com>
2018-11-05 17:26:50 +0530

    Code added for Overriding

--- src/main/java/com/java/oops/overriding/OverridingVariables.java
+++ src/main/java/com/java/oops/overriding/OverridingVariables.java
@@ -4,7 +4,7 @@ public class OverridingVariables
 {
 	// 1. Variable resolution is always taken care by compiler based on referenced type irrespective of whether the
 	// variable is static or non-static.
-	
+
 	// 2. Overriding concept applicable only for methods but not for variables.
 
 	public static void main(String[] args)
