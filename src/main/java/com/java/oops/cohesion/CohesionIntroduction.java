6de22a0957ee92c90ea8b59e449dd98374f8a512
Kunal Ashar <kunal.ashar@zycus.com>
2018-11-08 19:32:30 +0530

    Cohesion code added

--- src/main/java/com/java/oops/cohesion/CohesionIntroduction.java
+++ src/main/java/com/java/oops/cohesion/CohesionIntroduction.java
@@ -2,5 +2,12 @@ package com.java.oops.cohesion;
 
 public class CohesionIntroduction
 {
+	// 1. For every component a clear well-defined functionality is defined then that component is said to be follow
+	// high-cohesion.
+
+	// 2. High Cohesion is always a good programming practice because it has several advantages:
+	// i. Without affecting remaining components we can modify any component. Hence, enhancement will become easy.
+	// ii. It promotes reusability of the code. Where ever validation is required, we can reuse same code.
+	// iii. It improves maintainability of the application.
 
 }
