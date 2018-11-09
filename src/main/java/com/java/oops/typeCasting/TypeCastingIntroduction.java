61826315e4c09e936cc8c0d3fcc83fd5c27de098
Kunal Ashar <kunal.ashar@zycus.com>
2018-11-09 20:35:33 +0530

    type casting code added

--- src/main/java/com/java/oops/typeCasting/TypeCastingIntroduction.java
+++ src/main/java/com/java/oops/typeCasting/TypeCastingIntroduction.java
@@ -2,5 +2,43 @@ package com.java.oops.typeCasting;
 
 public class TypeCastingIntroduction
 {
+	// 1. We can use parent reference to hold child objects.
 
+	// Example
+	Object			o	= new String("typeCase");
+
+	// 2. We can use interface reference to hold implemented class object.
+
+	// Example
+	Runnable		r	= new Thread();
+
+	/** Syntax of Type Casting **/
+	// A b = (C) d;
+
+	// A > class or interface name.
+	// b > name of reference variable.
+	// C > class or interface name.
+	// d > reference variable name.
+
+	/** Compile time checking on above statement **/
+	// 1. The type of 'd' and 'C' must have some relation. Either child to parent or parent to child or same type
+	// otherwise we'll get compile time error saying: Inconvertible types Found: d type Required: C
+
+	// Example 1
+	StringBuffer	sb	= (StringBuffer) o;
+
+	// Example 2
+	String			s	= new String("typeCase");
+	StringBuffer	sb1	= (StringBuffer) s;
+	// CE: Inconvertible types Found: java.lang.String type Required:java.lang.StringBuffer
+
+	// 2. 'C' must be either same or derived type of 'A' otherwise we'll get compile time error saying:
+	// CE: Incompatible types Found: C Required: A
+
+	// Example 1
+	StringBuffer	sb2	= (StringBuffer) o;
+
+	// Example 2
+	StringBuffer	sb3	= (String) o;
+	// CE: Incompatible types Found: java.lang.String type Required:java.lang.StringBuffer
 }
