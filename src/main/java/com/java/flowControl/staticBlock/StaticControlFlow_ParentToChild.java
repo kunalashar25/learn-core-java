96913532eb1477c252a90b7ed5eff37605cb56fd
Kunal Ashar <kunal.ashar@zycus.com>
2018-11-21 14:31:46 +0530

    static block code added

--- src/main/java/com/java/flowControl/staticBlock/StaticControlFlow_ParentToChild.java
+++ src/main/java/com/java/flowControl/staticBlock/StaticControlFlow_ParentToChild.java
@@ -8,8 +8,8 @@ public class StaticControlFlow_ParentToChild
 	// 1. Identification of static members from parent to child.
 	// 2. Execution of static variable assignments and static blocks from parent to child.
 	// 3. Execution of only child class main method.
-	
-	// Refer StaticClassExecutionSequence_ParentToChild.PNG
+
+	// Refer StaticClassExecutionSequence_ParentToChild.PNG (steps as per Derived class main() method execution).
 
 	// Steps 1 to 11 from image falls under Step 1 from above sequence.
 	// Steps 12 to 22 from image falls under Step 2 from above sequence.
@@ -65,7 +65,7 @@ class Derived extends StaticControlFlow_ParentToChild
 
 	static int y = 200;
 
-	// Output:
+	/** Output when executed Derived class main(): **/
 	// 0
 	// Base Static Block
 	// 0
@@ -73,4 +73,14 @@ class Derived extends StaticControlFlow_ParentToChild
 	// Derived Second Static Block
 	// 200
 	// Derived Main
+
+	/** Output when executed StaticControlFlow_ParentToChild class main(): **/
+	// 0
+	// Base Static Block
+	// 20
+	// Main Method
+
+	// NOTE: Whenever we're loading child class automatically parent class will be loaded but whenever we're loading
+	// parent class, child class won't be loaded (because parent class members by default available to the child class
+	// whereas child class members by default won't be available to the parent).
 }
