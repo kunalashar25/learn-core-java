2c30888747ce1551c16f257dcbfd661ff71564ad
Ashar <Kunal.Ashar@nielsen.com>
2018-12-05 14:24:51 +0530

    static and Instance flow code added #63

--- src/main/java/com/java/flowControl/staticAndInstanceControlFlow/StaticAndInstanceControlFlow.java
+++ src/main/java/com/java/flowControl/staticAndInstanceControlFlow/StaticAndInstanceControlFlow.java
@@ -2,10 +2,46 @@ package com.java.flowControl.staticAndInstanceControlFlow;
 
 public class StaticAndInstanceControlFlow
 {
+	{
+		System.out.println("First Instance Block");
+	}
+
+	static
+	{
+		System.out.println("First Static Block");
+	}
+
+	public StaticAndInstanceControlFlow()
+	{
+		System.out.println("Constructor");
+	}
 
 	public static void main(String[] args)
 	{
+		StaticAndInstanceControlFlow s1 = new StaticAndInstanceControlFlow();
+		System.out.println("Main");
 
+		StaticAndInstanceControlFlow s2 = new StaticAndInstanceControlFlow();
+	}
+
+	static
+	{
+		System.out.println("Second Static Block");
+	}
+
+	{
+		System.out.println("Second Instance Block");
 	}
 
 }
+
+// Output:
+// First Static Block
+// Second Static Block
+// First Instance Block
+// Second Instance Block
+// Constructor
+// Main
+// First Instance Block
+// Second Instance Block
+// Constructor
