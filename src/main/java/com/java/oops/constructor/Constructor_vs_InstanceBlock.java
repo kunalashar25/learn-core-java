6d3d65177889ff85c928eabcffad5118278fbe6d
Ashar <Kunal.Ashar@nielsen.com>
2018-12-05 18:22:44 +0530

    constructor vs instance block code added #64

---
+++ src/main/java/com/java/oops/constructor/Constructor_vs_InstanceBlock.java
@@ -0,0 +1,11 @@
+package com.java.oops.constructor;
+
+public class Constructor_vs_InstanceBlock
+{
+	// 1. The main purpose of constructor is to perform initialization of an object but other than initialization if we
+	// want to perform any activity for every object creation then we should go for instance block (like updating entry
+	// in database for every object creation or incrementing counter for every object creation, etc).
+
+	// 2. Both constructor and instance block have their own different purposes and replacing one concept with another
+	// concept may not work always.
+}
