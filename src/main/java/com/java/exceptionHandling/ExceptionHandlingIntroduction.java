fd9842ad02fa808fcaa4cda65759e44280856c9b
kunalashar25 <kunalashar.25@gmail.com>
2018-12-25 15:53:37 +0530

    exception handling code added #68

---
+++ src/main/java/com/java/exceptionHandling/ExceptionHandlingIntroduction.java
@@ -0,0 +1,14 @@
+package com.java.exceptionHandling;
+
+public class ExceptionHandlingIntroduction
+{
+	// 1. An unexpected unwanted event that disturbs normal flow of the program is called an Exception.
+
+	// Example:
+	// 1. TyrePunctureException (Custom Exception)
+	// 2. SleepingException (Custom Exception)
+	// 3. FileNotFoundException
+
+	// 2. It is highly recommended to handle exceptions and main objective of exception handling is graceful
+	// termination of the program.
+}
