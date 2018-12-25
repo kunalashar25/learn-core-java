eba1d98a393d191ab87f285a0961fc20462b359b
kunalashar25 <kunalashar.25@gmail.com>
2018-12-25 17:12:36 +0530

    exception handling code added #68

--- src/main/java/com/java/exceptionHandling/ExceptionHandlingIntroduction.java
+++ src/main/java/com/java/exceptionHandling/ExceptionHandlingIntroduction.java
@@ -1,5 +1,7 @@
 package com.java.exceptionHandling;
 
+import java.io.FileNotFoundException;
+
 public class ExceptionHandlingIntroduction
 {
 	// 1. An unexpected unwanted event that disturbs normal flow of the program is called an Exception.
@@ -11,4 +13,24 @@ public class ExceptionHandlingIntroduction
 
 	// 2. It is highly recommended to handle exceptions and main objective of exception handling is graceful
 	// termination of the program.
+
+	// 3. Exception handling doesn't mean repairing an exception. We have to provide alternative way to continue rest of
+	// the program normally, is the concept of Exception Handling.
+
+	// Example:
+	// Our programming requirement is to read data from remote file location. At runtime, if remote file is not
+	// available then our program should not be terminated abnormally. We have to provide some local file to continue
+	// rest of the program normally. This way of defining alternative is nothing but exception handling.
+
+	public void smapleCode()
+	{
+		try
+		{
+			// read data from remote file
+		}
+		catch (FileNotFoundException e)
+		{
+			// use local file and continue rest of the program locally.
+		}
+	}
 }
---
+++ src/main/java/com/java/exceptionHandling/RuntimeStackMechanism.java
@@ -0,0 +1,34 @@
+package com.java.exceptionHandling;
+
+public class RuntimeStackMechanism
+{
+	// 1. For every thread JVM will create a runtime stack.
+
+	// 2. Each and every method call performed by that thread will be stored in a corresponding stack.
+
+	// 3. Each entry in the stack is called StackFrame or ActivationRecord.
+
+	// 4. After completing every method call, the corresponding entry from the stack will be removed.
+
+	// 5. After completing all method calls, the stack will become empty and empty stack will be destroyed by JVM just
+	// before terminating the thread.
+	
+	// Refer RuntimeStackMechanism.png
+
+	public static void main(String[] args)
+	{
+		doStuff();
+	}
+
+	public static void doStuff()
+	{
+		doMoreStuff();
+	}
+
+	public static void doMoreStuff()
+	{
+		System.out.println("Hello");
+	}
+}
+
+// Output: Hello
