35cc2aea73d089891aef3aa64f18c587de423073
Kunal Ashar <Kunal.Ashar@nielsen.com>
2019-02-20 17:11:37 +0530

    Customized exceptions code added #75

---
+++ src/main/java/com/java/exceptionHandling/customizedExceptions/CustomizedExceptions.java
@@ -0,0 +1,15 @@
+package com.java.exceptionHandling.customizedExceptions;
+
+public class CustomizedExceptions
+{
+	// 1. Sometimes to meet programming requirements we can define our own exceptions, Such type of exceptions are
+	// called, customized or user-defined exceptions.
+	
+	// Examples:
+	// TooYoungException
+	// TooOldException
+	// InsufficientFundException
+	
+	
+	
+}
