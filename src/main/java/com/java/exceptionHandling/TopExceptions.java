1797718319c232f7d99f823bbbbcfc056bc29649
Kunal Ashar <Kunal.Ashar@nielsen.com>
2019-02-21 15:21:13 +0530

    code added for common exceptions #76

---
+++ src/main/java/com/java/exceptionHandling/TopExceptions.java
@@ -0,0 +1,18 @@
+package com.java.exceptionHandling;
+
+public class TopExceptions
+{
+	// 1. Based on the person who is rising an exception, all exceptions are divided into two types:
+	// i. JVM Exceptions.
+	// ii. Programmatic Exceptions.
+
+	/** JVM Exception **/
+	// The exception which are raised automatically by JVM whenever a particular events occurs are called JVM
+	// exceptions.
+	// Examples: ArithmeticException, NullPointerException, etc.
+
+	/** Programmatic Exceptions **/
+	// The exceptions which are raised explicitly either by programmer or by API developer to indicate that something is
+	// wrong are called programmatic exceptions.
+	// Examples: TooOldException, TooYoungException, IllegalArgumentException, etc.
+}
