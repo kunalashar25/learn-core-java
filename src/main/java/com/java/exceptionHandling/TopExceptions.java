144eca15cfd38fccbc27835208f69a2a48c31f03
Kunal Ashar <Kunal.Ashar@nielsen.com>
2019-02-21 15:51:57 +0530

    code added for common exceptions #76

--- src/main/java/com/java/exceptionHandling/TopExceptions.java
+++ src/main/java/com/java/exceptionHandling/TopExceptions.java
@@ -15,4 +15,41 @@ public class TopExceptions
 	// The exceptions which are raised explicitly either by programmer or by API developer to indicate that something is
 	// wrong are called programmatic exceptions.
 	// Examples: TooOldException, TooYoungException, IllegalArgumentException, etc.
+
+	/** Top 10 Exception **/
+
+	// 1. ArrayIndexOutOfBoundsException
+	// i. It is a child class of RuntimeException and hence it's unchecked exception.
+	// ii. Raised automatically by JVM whenever we're trying to access Array elements without out of range index.
+
+	// Example
+	public void m1()
+	{
+		int[] x = new int[4];
+		System.out.println(x[0]);
+		System.out.println(x[10]); // Exception
+		System.out.println(x[-5]); // Exception
+	}
+
+	// 2. NullPointerException
+	// i. It is a child class of RuntimeException and hence it's unchecked exception.
+	// ii. Raised automatically by JVM whenever we're trying to perform any operation on null.
+
+	// Example
+	public void m2()
+	{
+		String s = null;
+		System.out.println(s.length()); // Exception
+	}
+
+	// 3. ClassCastException
+	// i. It is a child class of RuntimeException and hence it's unchecked exception.
+	// ii. Raised automatically by JVM whenever we're trying to typecast parent object to child type.
+
+	// Example
+	public void m3()
+	{
+		Object o = new Object();
+		String s = (String) o; // Exception
+	}
 }
