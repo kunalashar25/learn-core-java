9c0384601e22c82eae96eb37be951bd37a2d9e17
Kunal Ashar <Kunal.Ashar@nielsen.com>
2019-03-04 15:50:03 +0530

    exceptionh andling concepts added code added #77

---
+++ src/main/java/com/java/exceptionHandling/introduction/ExceptionHandlingWords.java
@@ -0,0 +1,25 @@
+package com.java.exceptionHandling.introduction;
+
+public class ExceptionHandlingWords
+{
+	/** Exception Propagation **/
+	// Inside a method if an exception is raised and if we're not handling that exception then
+	// exception object will be propagated to caller then caller method is responsible to handle exception. This process
+	// is called exception propagation
+
+	/** Rethrowing Exception **/
+	// We can use this approach to convert one exception type to another exception type.
+
+	// Example
+	public void m1()
+	{
+		try
+		{
+			System.out.println(10 / 0);
+		}
+		catch (ArithmeticException e)
+		{
+			throw new NullPointerException();
+		}
+	}
+}
