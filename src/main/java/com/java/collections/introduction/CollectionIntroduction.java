95c770cee5261c822d58e2d18a7739dc5881db80
Kunal Ashar <Kunal.Ashar@nielsen.com>
2019-03-05 12:27:23 +0530

    collection introduction code added #132

---
+++ src/main/java/com/java/collections/introduction/CollectionIntroduction.java
@@ -0,0 +1,21 @@
+package com.java.collections.introduction;
+
+public class CollectionIntroduction
+{
+	/** Arrays Overview **/
+	// 1. An Array is an indexed collection of fixed number of homogeneous data elements.
+
+	// 2. The main advantage of arrays is we can represent multiple values by using single variable so that readability
+	// of the code will be improved.
+
+	/** Limitations of Arrays **/
+	// 1. Arrays are fixed in size i.e once we create an array there is no way to increase or decrease array size based
+	// on our requirement. Due to this, to use arrays concept we must know the data size in advance which is not
+	// possible in all the cases.
+
+	// 2. Array can hold only homogeneous data type elements.
+
+	// 3. Arrays concept is not implemented based on some standard data structure and hence ready made method support is
+	// not available. For every requirement, we have to write the code explicitly which increases complexity of the
+	// programming.
+}
