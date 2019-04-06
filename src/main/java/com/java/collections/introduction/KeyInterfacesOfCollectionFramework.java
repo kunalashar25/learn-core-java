0033b50a8c7eaf057986e50dece1f51f5d9f0584
kunal ashar <kunalashar.25@gmail.com>
2019-04-06 15:53:29 +0800

    collection overview completed #133

--- src/main/java/com/java/collections/introduction/KeyInterfacesOfCollectionFramework.java
+++ src/main/java/com/java/collections/introduction/KeyInterfacesOfCollectionFramework.java
@@ -64,4 +64,52 @@ public class KeyInterfacesOfCollectionFramework
 	// Example: Before sending a mail all mail id's we have to store in some data structure. In which order we added
 	// mail id's in the same order only mail should be delivered. For this requirement, queue is the best choice.
 
+	// Note: All the above interfaces (#1-6) meant for representing a group of individual objects. If we want to
+	// represent a group of object as key-value pairs then we should go for map.
+
+	/** 7. Map Interface **/
+	// 1. Map is NOT a child interface of collection.
+
+	// 2. If we want to represent a group of objects as key-value pairs then we should go for map.
+
+	// 3. Both key and value are objects only. Duplicate keys are not allowed but values can be duplicated.
+
+	// 4. Implementation classes are: HashMap, LinkedHashMap, WeakHashMap, IdentityHashMap, Hashtable, Properties.
+
+	/** 8. SortedMap Interface **/
+	// 1. It is a child interface of Map interface.
+
+	// 2. If we want to represent a group of key-values pairs according to some sorting order of keys, then we should go
+	// for sorted map.
+
+	// 3. In Sorted Map, the sorting should be based on key but not based on value.
+
+	/** 9. Navigable Map Interface **/
+	// 1. It is a child interface of Sorted Map interface.
+
+	// 2. It defines several method for navigation purposes.
+
+	// 3. TreeMap is implementation class of Navigable Map.
+
+	// Note: The following are legacy characters present in collection framework:
+	// i. Enumeration (I)
+	// ii. Dictionary (AC)
+	// iii. Vector (C)
+	// iv. Stack (C)
+	// v. Hashtable (C)
+	// vi. Properties (C)
+
+	/** Sorting **/
+	// i. Comparable (I): for default natural sorting.
+	// ii. Comparator (I): for customized sorting.
+
+	/** Cursors **/
+	// used to fetch object from collections one by one.
+	// i. Enumeration (I)
+	// ii. Iterator (I)
+	// iii. ListIterator (I)
+
+	/** Utility Classes **/
+	// i. Collections
+	// ii. Arrays
 }
