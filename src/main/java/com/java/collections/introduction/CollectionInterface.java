1eddf65b958c34479d274287b8e6cde8745e62fb
kunal ashar <kunalashar.25@gmail.com>
2019-04-06 16:35:18 +0800

    connection interface overview added #134

---
+++ src/main/java/com/java/collections/introduction/CollectionInterface.java
@@ -0,0 +1,10 @@
+package com.java.collections.introduction;
+
+public class CollectionInterface
+{
+	// 1. If we want to represent a group of individual objects as a single entity then we should go for collection.
+	
+	// 2. Collection interface defines the most common methods which are applicable for any collection object.
+	
+	// Note: There is no concrete class which implements collection interface directly.
+}
