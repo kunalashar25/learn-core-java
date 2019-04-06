b9f40de8746c09a786da8ba491bb03ff911e32fd
kunal ashar <kunalashar.25@gmail.com>
2019-04-06 17:07:27 +0800

    list interface overview added #134

---
+++ src/main/java/com/java/collections/list/ListInterface.java
@@ -0,0 +1,24 @@
+package com.java.collections.list;
+
+public class ListInterface
+{
+	// 1. List is a child interface of collection.
+
+	// 2. If we want to represent a group of individual objects as a single entity where duplicates are allowed and
+	// insertion order must be preserved then we should go for list.
+
+	// 3. We can preserve insertion order via index and we can differentiate duplicate objects by using index. Hence,
+	// index will play very important role in List.
+
+	// 4. List interface defines the following specific methods:
+	// i. void add (int index, Object o)
+	// ii. boolean addAll(int index, Collection c)
+	// iii. Object get(int index)
+	// iv. Object remove (int index)
+	// v. Object set (int index, Object new) to replace object at specified index.
+	// vi. int indexOf (Object o)
+	// vii. int lastIndexOf (Object o)
+	// viii. ListIterator listIterator();
+	
+	// Start from 33.30 #134
+}
