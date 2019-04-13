f6f29d866715988879d17aecffc4e1e0c6fee5c9
kunal ashar <kunalashar.25@gmail.com>
2019-04-13 16:20:05 +0800

    ArrayList code added #135

--- src/main/java/com/java/collections/list/ArrayListIntroduction.java
+++ src/main/java/com/java/collections/list/ArrayListIntroduction.java
@@ -1,6 +1,11 @@
 package com.java.collections.list;
 
+import java.io.Serializable;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.RandomAccess;
 
 public class ArrayListIntroduction
 {
@@ -55,7 +60,54 @@ public class ArrayListIntroduction
 
 		// Note: in this case we'll get compile time warning to compile code via -xlint as we're not using generics
 		// while creating ArrayList.
-
 	}
 
+	// 4. Usually we can use collections to hold and transfer objects from one location to another location (container).
+	// To provide support for this requirement every collection class by default implements serializable and cloneable
+	// interfaces.
+
+	// 5. ArralList and Vector classes implements RandomAccess interface so that any random element can be accessed with
+	// the same speed.
+
+	// 6. RandomAccess interface is present in java.util package and it doesn't contain any methods. It is a marker
+	// interface, where required ability will be provided automatically by the JVM.
+
+	public static void interviewQuestions()
+	{
+		ArrayList l1 = new ArrayList<>();
+		LinkedList l2 = new LinkedList<>();
+
+		System.out.println(l1 instanceof Serializable); // Output: true
+		System.out.println(l1 instanceof Cloneable); // Output: true
+		System.out.println(l1 instanceof RandomAccess); // Output: true
+
+		System.out.println(l2 instanceof Serializable); // Output: true
+		System.out.println(l2 instanceof Cloneable); // Output: true
+		System.out.println(l2 instanceof RandomAccess); // Output: false
+	}
+
+	// 7. ArrayList is the best choice if our frequent operation is retrieval operation because ArrayList implement
+	// RandomAccess interface.
+
+	// 8. ArrayList is the worst choice if our frequent operation is insertion/deletion of objects in middle index.
+
+	// Refer Image ArrayList_vs_Vector.PNG for Differences between ArrayList and Vector.
+
+	/** How to get Synchronized version of ArrayList Object? **/
+	// By Default ArrayList is non-synchronized but we can get synchronized version of ArrayList object by using
+	// synchronizedList() method of Collections class.
+
+	// Method Signature: public static List synchronizedList(List l)
+
+	// Example
+	public void synchronizedListExample()
+	{
+		ArrayList list = new ArrayList<>();
+		List l1 = Collections.synchronizedList(list);
+	}
+
+	// Similarly, we can get synchronized version of Set and Map objects by using the following methods of Collections
+	// class:
+	// Method Signature: public static Set synchronizedSet(Set s)
+	// Method Signature: public static Map synchronizedMap(Map m)
 }
