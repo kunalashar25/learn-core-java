30e895a2a36e93e4bab33031ed9dc9c95a081ac4
kunal ashar <kunalashar.25@gmail.com>
2019-04-18 20:02:57 +0800

    TreeSet introduction added #139

---
+++ src/main/java/com/java/collections/set/TreeSetIntroduction.java
@@ -0,0 +1,22 @@
+package com.java.collections.set;
+
+public class TreeSetIntroduction
+{
+	// 1. The underlying data structure is BalancedTree.
+
+	// 2. Duplicate objects are not allowed.
+
+	// 3. Insertion order not preserved.
+
+	// 4. Heterogeneous objects are not allowed otherwise we'll get runtime exception saying ClassCastException.
+
+	// 5. null insertion is possible (only once).
+
+	// 6. TreeSet implements Serializable and Cloneable interfaces but not RandomAccess interface.
+
+	// 7. All objects will be inserted based on some sorting order. It may be default natural sorting order or
+	// customized sorting order.
+
+	/** Constructors **/
+	// Start from 7:10
+}
